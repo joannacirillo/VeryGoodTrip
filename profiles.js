970b4a32ba57577f7917c80ff31f1128cc7b5b9f
joannacirillo <61593541+joannacirillo@users.noreply.github.com>
2020-05-21 11:49:24 +0200

    Add files via upload

---
+++ profiles.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const userScheme = mongoose.Schema({
+    user_id : String,
+    cluster : Array,
+    interests : Array,
+    cuisine : Array,
+    historical : Array,
+    disability : Boolean,
+    previous : Array
+});
+
+module.exports = mongoose.model('profiles',userScheme,"profiles");
